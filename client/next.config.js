761d5359648f757024c590cf6a5696f84eb04ca4
Glade <gladekettle@gmail.com>
2019-04-18 14:43:54 +1000

    Fixed env vars

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,17 @@
 if (process.env.NODE_ENV !== 'production') {
   const ne = require('now-env')
-} // Ensure now env is loaded locally
+  console.log('process.env.API_URL:')
+  console.log(process.env.API_URL)
+}
 
-module.exports = {
+let conf = {
   target: 'serverless',
   env: {
-    API_URL: process.env.API_URL,
+    API_URL: process.env.API_URL || nowConf.env.API_URL,
   },
 }
+
+console.log('Launching with next config:')
+console.log(conf)
+
+module.exports = conf
