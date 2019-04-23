ed1d2b0c747c7619aa37692c2160be67ca6f2558
Glade <gladekettle@gmail.com>
2019-04-23 13:22:29 +1000

    Cleaned package exposure to getInitialProps

--- client/server/local.js
+++ client/server/local.js
@@ -6,18 +6,9 @@ const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev, conf: config })
 const handle = app.getRequestHandler()
-const fs = require('fs')
-const path = require('path')
 
 app.prepare().then(() => {
   const server = express()
-
-  server.use((req, res, next) => {
-    // now I can access this module inside the getInitialProps function
-    req.fs = fs
-    req.fsPath = path
-    return next()
-  })
 
   server.get('/departures/:stop_id', (req, res) => {
     console.log(req.params.stop_id)
