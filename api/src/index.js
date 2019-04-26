806735b089c60fbf44e3edd2c506a4768303dfed
Glade <gladekettle@gmail.com>
2019-04-26 17:21:25 +1000

    Tyepdefs in seperate .gql file. Deprecated unused deps.

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,14 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+// File / path
+const fs = require('fs')
+const path = require('path')
+
+// Types
+const typeDefs = gql(
+  fs.readFileSync(path.join(__dirname, './typedefs.gql'), 'utf-8')
+)
+
 // Require the module
 const createGraphQLLogger = require('graphql-log')
 const resolvers = require('./resolvers')
@@ -16,82 +25,6 @@ const logExecutions = createGraphQLLogger()
 
 // Wrap your resolvers
 logExecutions(resolvers)
-
-const typeDefs = gql`
-  type Stop {
-    stop_id: ID!
-    stop_suburb: String
-    route_type: Int
-    stop_longitude: Float
-    stop_latitude: Float
-    departures: [Departure]
-    stop_name: String
-    directions: [Direction]
-  }
-
-  type Stops {
-    stops: [Stop]
-  }
-
-  type Direction {
-    direction_id: Int
-    direction_name: String
-    route_id: String
-    route_type: String
-  }
-
-  type Directions {
-    directions: [Direction]
-  }
-
-  type Disruption {
-    disruption_id: Int
-    title: String
-    url: String
-    description: String
-    disruption_status: String
-    disruption_type: String
-    published_on: String
-    last_updated: String
-    from_date: String
-    to_date: String
-    colour: String
-    display_on_board: Boolean
-    display_status: Boolean
-  }
-
-  type Departure {
-    stop_id: ID!
-    route_id: Int
-    route_type: Int
-    stop_suburb: String
-    stop_name: String
-    run_id: Int
-    direction_id: Int
-    departure_sequence: Int
-    disruptions: [Disruption]
-    scheduled_departure_utc: String
-    estimated_departure_utc: String
-    platform_number: Int
-    direction_name: String
-  }
-
-  type Departures {
-    stop: Stop
-    departures: [Departure]
-    directions: [Direction]
-  }
-
-  type Query {
-    stops(search_term: String): [Stop]
-    stop(stop_id: Int): Stop
-    departures(stop_id: Int): [Departure]
-  }
-
-  schema {
-    query: Query
-  }
-`
 
 const server = new ApolloServer({
   typeDefs,
--- api/src/ptv/departures/api.js
+++ api/src/ptv/departures/api.js
@@ -9,10 +9,18 @@ class DeparturesAPI extends RESTDataSource {
   }
 
   async getDeparturesById(stop_id, maxResults = 5) {
-    console.log(`${this.baseURL}${ptvSig.pathWithSig(`/v3/departures/route_type/0/stop/${stop_id}`, [{ name: 'max_results', value: maxResults }, {name: 'expand', value: 'all'}], config.DEV_ID, config.DEV_KEY)}`)
-    return await this.get(`${this.baseURL}${ptvSig.pathWithSig(`/v3/departures/route_type/0/stop/${stop_id}`, [{ name: 'max_results', value: maxResults }, {name: 'expand', value: 'all'}], config.DEV_ID, config.DEV_KEY)}`)
+    return await this.get(
+      `${this.baseURL}${ptvSig.pathWithSig(
+        `/v3/departures/route_type/0/stop/${stop_id}`,
+        [
+          { name: 'max_results', value: maxResults },
+          { name: 'expand', value: 'all' },
+        ],
+        config.DEV_ID,
+        config.DEV_KEY
+      )}`
+    )
   }
-
 }
 
 module.exports = DeparturesAPI
