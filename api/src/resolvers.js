86446be82861a50a19d6b1e40f867bf02538bfff
Glade <gladekettle@gmail.com>
2019-05-13 21:50:41 +1000

    Unique short id added to disruption

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,30 +1,32 @@
-/**
- * Helper to extract all direction id's in given departure result
- * @param {Array} departures 
- */
-const extractDirectionIdFromDepartures = (departures) => (
-  departures.reduce((accumulator, curVal) => {
-    if (accumulator.indexOf(curVal.direction_id) === -1) accumulator.push(curVal.direction_id)
-    return accumulator
-  }, [])
-)
+const shortid = require('shortid')
 
 /**
  * Helper to extract all direction id's in given departure result
- * @param {Array} departures 
+ * @param {Array} departures
  */
-const extractRouteIdFromDepartures = (departures) => (
+const extractDirectionIdFromDepartures = departures =>
   departures.reduce((accumulator, curVal) => {
-    if (accumulator.indexOf(curVal.route_id) === -1) accumulator.push(curVal.route_id)
+    if (accumulator.indexOf(curVal.direction_id) === -1)
+      accumulator.push(curVal.direction_id)
     return accumulator
   }, [])
-)
+
+/**
+ * Helper to extract all direction id's in given departure result
+ * @param {Array} departures
+ */
+const extractRouteIdFromDepartures = departures =>
+  departures.reduce((accumulator, curVal) => {
+    if (accumulator.indexOf(curVal.route_id) === -1)
+      accumulator.push(curVal.route_id)
+    return accumulator
+  }, [])
 
 /**
  * Helper to extract stop data from departures API with expand=all activated on api
- * @param {Array} departures 
+ * @param {Array} departures
  */
-const extractStopFromDepartures = (departures) => {
+const extractStopFromDepartures = departures => {
   const id = departures.departures[0].stop_id // Grab stop id from first dep
   return departures.stops[id]
 }
@@ -33,7 +35,7 @@ const extractStopFromDepartures = (departures) => {
  * Helper to extract stop data from departures API with expand=all activated on api
  * @param {Array} departures
  */
-const extractDirectionsFromDepartures = (departures) => {
+const extractDirectionsFromDepartures = departures => {
   const directions = []
 
   for (let d = 0; d < departures.directions.length; d++) {
@@ -48,11 +50,8 @@ const extractDirectionsFromDepartures = (departures) => {
  * @param {String} direction_id
  * @param {Array} directions
  */
-const extractDirectionByDirectionId = (direction_id, directions) => (
-  directions.filter((val) => (
-    val.direction_id === direction_id
-  ))
-)
+const extractDirectionByDirectionId = (direction_id, directions) =>
+  directions.filter(val => val.direction_id === direction_id)
 
 const resolvers = {
   Query: {
@@ -64,26 +63,43 @@ const resolvers = {
     // Query for stop id
     stop: async (_, { stop_id }, { dataSources }) => {
       // Stitch together some sane data
-      
-      let departures = await dataSources.departuresAPI.getDeparturesById(stop_id)
+
+      let departures = await dataSources.departuresAPI.getDeparturesById(
+        stop_id
+      )
       let stop = {
         ...extractStopFromDepartures(departures),
         directions: [],
-        departures: []
+        departures: [],
       }
 
       stop.directions = extractDirectionsFromDepartures(departures) || []
 
+      let tmpDisruption = {}
       // 1 - Stitch up departure data for easy UI consumption
       for (let dep = 0; dep < departures.departures.length; dep++) {
-        console.log("Direction ID:")
+        console.log('Direction ID:')
         console.log(departures.departures[dep].direction_id)
-        departures.departures[dep].direction_name = departures.directions[departures.departures[dep].direction_id].direction_name
+        departures.departures[dep].direction_name =
+          departures.directions[
+            departures.departures[dep].direction_id
+          ].direction_name
         departures.departures[dep].disruptions = []
 
         // Loop over disruptions data
-        for (let dis = 0; dis < departures.departures[dep].disruption_ids.length; dis++) {
-          departures.departures[dep].disruptions.push(departures.disruptions[departures.departures[dep].disruption_ids[dis]])
+        for (
+          let dis = 0;
+          dis < departures.departures[dep].disruption_ids.length;
+          dis++
+        ) {
+          tmpDisruption = {
+            ...departures.disruptions[
+              departures.departures[dep].disruption_ids[dis]
+            ],
+            sid: shortid.generate(),
+          }
+
+          departures.departures[dep].disruptions.push(tmpDisruption)
         }
       }
 
@@ -92,10 +108,10 @@ const resolvers = {
       return {
         ...stop,
         directions: departures.directions,
-        departures: departures.departures
+        departures: departures.departures,
       }
-    }
-  }
+    },
+  },
 }
 
 if (module) module.exports = resolvers
